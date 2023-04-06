efdb4a0d854d17af5d50414514f9c16c7167c68d
Heechan <ckstmznf@naver.com>
2023-04-07 01:44:24 +0900

    Change wallpaper

--- main.py
+++ main.py
@@ -2,14 +2,14 @@ import glob
 import os
 import random
 
-import win32api
-import win32con
-import win32gui
+import ctypes
 
 from EmptyWallpaperFile import EmptyWallpaperFile
 
+
 def relative_path_to_absolute_path(relative_path):
     return os.path.abspath(relative_path)
+
 
 def get_fake_wallpaper():
     fake_wallpapers = glob.glob("./assets/fake/*")
@@ -35,25 +35,25 @@ def get_real_wallpaper():
 
 def setup_wallpaper(wallpaper_path):
     abs_path = relative_path_to_absolute_path(wallpaper_path)
-
-    key = win32api.RegOpenKeyEx(win32con.HKEY_CURRENT_USER, "Control Panel\\Desktop", 0, win32con.KEY_SET_VALUE)
-    win32api.RegSetValueEx(key, "Wallpaper", 0, win32con.REG_SZ, abs_path)
-    win32api.RegCloseKey(key)
-
-    print(abs_path)
-
-    win32gui.SystemParametersInfo(win32con.SPI_SETDESKWALLPAPER, abs_path, win32con.SPIF_SENDWININICHANGE)
-
+    ctypes.windll.user32.SystemParametersInfoW(20, 0, abs_path, 3)
 
 def reset_background():
     try:
         wallpaper = get_fake_wallpaper()
 
     except EmptyWallpaperFile:
-        print("./assest/real 파일안에 real배경화면을 넣어주세요.")
+        print("./assest/fake 파일안에 fake배경화면을 넣어주세요.")
 
     setup_wallpaper(wallpaper)
 
+def show_real_background():
+    try:
+        wallpaper = get_real_wallpaper()
+
+    except EmptyWallpaperFile:
+        print("./assest/real 파일안에 real배경화면을 넣어주세요.")
+
+    setup_wallpaper(wallpaper)
 
 if __name__ == '__main__':
     reset_background()
